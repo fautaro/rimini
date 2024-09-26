82cd2c53f7db2aa1ccbd6c7046244a825678ba01
fautaro <fautaro@gmail.com>
2024-09-26 17:23:28 -0300

    Add controllers - fix layout

---
+++ AdminPanel/MVC/Controllers/CategoriasController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers;
+
+public class CategoriasController : Controller
+{
+    #region GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Nuevo()
+    {
+        return View();
+    }
+    public IActionResult Editar()
+    {
+        return View();
+    }
+
+    public IActionResult Desactivar()
+    {
+        return View();
+    }
+    #endregion
+
+    #region POST
+
+    #endregion
+}

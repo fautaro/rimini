70403d2497cba394e98cbe3c89b80acec74e63a1
fautaro <fautaro@gmail.com>
2024-09-26 21:31:45 -0300

    Avances en maquetación

--- AdminPanel/src/Domain/Entities/User.cs
+++ AdminPanel/src/Domain/Entities/User.cs
@@ -4,13 +4,13 @@
     {
         public int Id { get; set; }
         public DateTime Created { get; set; }
-        public string Email { get; set; }
-        public string First_Name { get; set; }
-        public string Last_Name { get; set; }
-        public string Password { get; set; }
+        public string? Email { get; set; }
+        public string? First_Name { get; set; }
+        public string? Last_Name { get; set; }
+        public string? Password { get; set; }
         public DateTime Updated { get; set; }
-        public string Username { get; set; }
-        public string Role { get; set; }
+        public string? Username { get; set; }
+        public string? Role { get; set; }
         public bool Active { get; set; }
 
     }
